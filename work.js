877bcd946bcaeb6250644cad25a1435c4d253225
Faizanhaider461 <syedfaizan768768@gmail.com>
2025-03-13 01:48:06 +0500

    Add files via upload

---
+++ work.js
@@ -0,0 +1,42 @@
+let a= document.querySelector("img")
+a.style.width = "45%"
+a.style.height = "50vh"
+a.style.borderColor = "Black"
+
+
+function Img1(){
+    a.src= "cloudy park.jpg"
+    
+}
+
+function Img2(){
+    a.src= "sky1.webp"
+    
+
+}
+
+function Img3(){
+    a.src= "sky2.jpg"
+
+}
+
+function Img4(){
+    a.src= "sky3.jpg"
+
+}
+
+function Img5(){
+    a.src= "sky4.jpg"
+
+}
+
+function Img6(){
+    a.src= "sky5.jpg"
+
+}
+
+
+
+
+
+
