e57e99964c4e73c4162688582d723f335520547b
Faizanhaider461 <syedfaizan768768@gmail.com>
2025-03-13 05:22:27 +0500

    Add files via upload

--- work.js
+++ work.js
@@ -1,37 +1,37 @@
 let a= document.querySelector("img")
 a.style.width = "45%"
-a.style.height = "50vh"
+a.style.height = "55vh"
 a.style.borderColor = "Black"
 
 
 function Img1(){
-    a.src= "cloudy park.jpg"
+    a.src= "Babar shot.jpg"
     
 }
 
 function Img2(){
-    a.src= "sky1.webp"
+    a.src= "b1.jpg"
     
 
 }
 
 function Img3(){
-    a.src= "sky2.jpg"
+    a.src= "b4.jpg"
 
 }
 
 function Img4(){
-    a.src= "sky3.jpg"
+    a.src= "b2.webp"
 
 }
 
 function Img5(){
-    a.src= "sky4.jpg"
+    a.src= "b3.webp"
 
 }
 
 function Img6(){
-    a.src= "sky5.jpg"
+    a.src= "b5.webp"
 
 }
 
